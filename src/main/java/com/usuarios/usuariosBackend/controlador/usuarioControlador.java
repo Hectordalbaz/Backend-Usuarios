b98078a3da9bb6bea0a69decc0c50957e733d2fd
Hectordalbaz <hectordalbaz@gmail.com>
2024-06-16 19:35:40 -0600

    Se agrega clase controladora de usuarios

--- src/main/java/com/usuarios/usuariosBackend/controlador/usuarioControlador.java
+++ src/main/java/com/usuarios/usuariosBackend/controlador/usuarioControlador.java
@@ -1,14 +1,27 @@
 package com.usuarios.usuariosBackend.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.usuarios.usuariosBackend.modelo.Usuario;
+import com.usuarios.usuariosBackend.repositorio.UsuarioRepositorio;
 
 @RestController
 @RequestMapping("")
 @CrossOrigin(origins = "http://localhost:4200")
 public class usuarioControlador {
+	@Autowired
+	private UsuarioRepositorio repositorio;
 	
+	@GetMapping("/usuarios")
+	public List<Usuario> listarUsuarios(){
+		return repositorio.findAll();
+	}
 	
 
 }
